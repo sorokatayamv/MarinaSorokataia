1ff719af508fe42c92b74656eb4d23b016d19260
С М <sorokat-marinad@mail.ru>
2021-05-07 09:57:46 +0300

    Add target in gitignore file. Add new test base class with before and after methods. Extend test base class with TestSum, TestSubtract, TestDivide, TestMultiply classes. Removed extra dependency in pom.xml

--- src/test/java/ru/training/at/hw1/TestMultiply.java
+++ src/test/java/ru/training/at/hw1/TestMultiply.java
@@ -6,12 +6,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TestMultiply {
-    private Calculator calculator = new Calculator();
+public class TestMultiply extends TestBaseClass{
 
     @BeforeMethod
-    public void beforeMethod(){
-        calculator = new Calculator();
+    @Override
+    public void beforeMethod() {
+        super.beforeMethod();
     }
 
     @Test(groups = {"multAndDiv"},dataProvider = "dataProviderMult", dataProviderClass = DataProviderClass.class)
@@ -21,7 +21,8 @@ public class TestMultiply {
     }
 
     @AfterMethod
-    public void afterMethod(){
-        calculator = null;
+    @Override
+    public void afterMethod() {
+        super.afterMethod();
     }
 }
